c14d26c91769e98eb66177943bc5b6ac914b331b
Davi Cecílio <davim.cecilio@gmail.com>
2020-06-25 19:38:40 -0300

    Updates

---
+++ precache-manifest.759da61049079f8581f4e3da5ee4a829.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85e409ac6796db8f7b93096536fb96a5",
+    "url": "/gitcompare-hooks/index.html"
+  },
+  {
+    "revision": "f394d45dc16482be10bd",
+    "url": "/gitcompare-hooks/static/css/2.b09600e4.chunk.css"
+  },
+  {
+    "revision": "f394d45dc16482be10bd",
+    "url": "/gitcompare-hooks/static/js/2.d750bb99.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/gitcompare-hooks/static/js/2.d750bb99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bae0d3b27a0e77410bc6",
+    "url": "/gitcompare-hooks/static/js/main.7c6e4b66.chunk.js"
+  },
+  {
+    "revision": "a928bc78d028e78686d2",
+    "url": "/gitcompare-hooks/static/js/runtime-main.0eea2f1f.js"
+  },
+  {
+    "revision": "b6ad0874ddb8d7033b5887c9d5884e3d",
+    "url": "/gitcompare-hooks/static/media/loaderIcon.b6ad0874.svg"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/gitcompare-hooks/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/gitcompare-hooks/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/gitcompare-hooks/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/gitcompare-hooks/static/media/notification.c0d3c94c.eot"
+  }
+]);
